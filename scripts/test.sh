b29f1bfa8c6566b7bdf352e516b1a13385e44088
강동윤 <kdy1997.dev@gmail.com>
2020-08-23 17:38:31 +0900

    Let's use perf

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,6 +10,8 @@ ls -al target/release/spack-cli
 
 time -p target/release/spack-cli  rxjs/src/internal/observable/dom/AjaxObservable.ts -d out
 
+sudo perf record --call-graph=dwarf ./target/release/spack-cli
 # sudo cargo flamegraph --bin spack-cli
-sudo flamegraph target/release/spack-cli 
+# sudo flamegraph target/release/spack-cli 
 
+perf report
